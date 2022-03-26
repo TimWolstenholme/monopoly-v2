2d947af595fbe1b7e2d9ed2eba733d0048b9dc45
Tim Wolstenholme <timwolstenholme06@gmail.com>
2022-03-26 21:52:05 +0000

    Added a method to the game class to buy a property Added a method to shift to the next player Minor bug fixes

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from board import *
 class Player:
-    def __init__(self):
+    def __init__(self,next=None):
         self.money=2000
         self.property=[]
         self.mortgages=[]
         self.in_jail=False
         self.board_locations=monopoly_board.first_square
         self.location_name=self.board_locations.name
+        self.next=None
 class PlayerChain:
-    def __init__(self,first_player):
+    def __init__(self,first_player:Player):
         self.first_player=first_player
+        self.cur_player=self.first_player
     def add_player(self,new_player:Player):
         cur=self.first_player
         while cur.next:
@@ -22,5 +24,14 @@ class PlayerChain:
         cur.next=self.first_player
 
 class Game:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,players:PlayerChain) -> None:
+        self.end=False
+        self.players=players
+    def buy_property(self):
+        self.players.cur_player.property.append(self.players.cur_player.board_locations.name)
+        self.players.cur_player.money-=self.players.cur_player.board_locations.price
+        print(f"{self.players.cur_player.board_locations} bought for {self.players.cur_player.board_locations.price}")
+    def next_player(self):
+        self.players.cur_player=self.players.cur_player.next
+    
+
